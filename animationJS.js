92cf068063cecaf6a144858f5fa20c4f17174531
CzytelnyNick <odrowazd@gmail.com>
2022-05-03 22:41:07 +0200

    animation

--- animationJS.js
+++ animationJS.js
@@ -4,11 +4,11 @@ const szerokosc = 128;
 const wysokosc = 138;
 let klatka = 0;
 
+const sprite = new Image();
 
-function draw() {
-        // setInterval(() =>{
-
-            requestAnimationFrame(draw)
+        const fps = 15;
+        function forward() {
+          ctx.drawImage(sprite, 100, 0)
             ctx.clearRect(0, 0, canvas.width, canvas.height)
             if(klatka == 8)
             klatka = 0;
@@ -16,15 +16,26 @@ function draw() {
             ctx.drawImage(sprite, aktKlatka, 0, szerokosc, wysokosc,canvas.width / 5, canvas.height - 300, szerokosc - 10, wysokosc)
             klatka++;
             console.log(aktKlatka);
-        // }, 1000/10)
-    
+          setTimeout(() => {
+            
+         requestAnimationFrame(forward);
+          }, 1000 / fps);
+        }
+        
   
-  }
-  const sprite = new Image();
+  
+function  backward()
+{
+
+}
+  
   sprite.src = "stripe.png"
-window.addEventListener("load", () => {
-draw();
+window.addEventListener("click", () => {
+forward();
      })
+window.addEventListener("contextmenu", () => {
+backward()
+})
 
 
   
